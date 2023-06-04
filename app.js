4d78d7f2a10aa43b6978af5fc98423329e9773c0
anumaslam-789 <130720606+anumaslam-789@users.noreply.github.com>
2023-06-05 00:27:03 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,123 @@
+
+var correctPassword = "anum aslam";
+var userPassword = prompt("please enter your password Ayan!");
+if((userPassword === null )||( userPassword === "")); {
+
+     alert(" please enter your password yaar!");};
+
+ {  alert("Password is enter,Now you can Proceed");}
+   
+ if(userPassword === correctPassword) {
+     
+     alert("your password is correct ayan you got it!");
+   
+}
+
+   else{
+     
+     console.log("Incorrect Password,Please try again.");
+
+}
+   //var age = "I am 15 Years Old";
+   //alert(age);
+   //var time = "you Have Visited This Sites 14 Times";
+  // alert(time);
+ // var birthYear = "My birth year is 1990";
+ // document.write(birthYear);
+// var visitorName = "john Doe";
+// var productTitle = " on Anum & company";
+// var quantity = " //Ordered 5 T-shirt(s)";
+// document.write(visitorName+productTitle+quantity);
+//   Assingnmnt # 5
+
+//var sum = (3+5);
+//alert(sum);
+//var sub =(3-5);
+//alert(sub);
+//var multiply =(3*5);
+//alert(multiply);
+//var division = (3%5);
+//alert(division);
+//var num = 5;
+//++num
+//var num2 = 7
+//alert(num-- + num2);
+ //document.write(num);
+ 
+
+                    // MOVIE tickets//
+ //var price = 600 ;
+//var tickets = 5;
+//alert((price*tickets) + "PKR"); 
+            
+                 // " FORENHEIT AND CENCIUS"
+//var calcius = "25" ;
+//var forenheit  = (calcius*9/5) + 32;
+//alert("25F+is+forenheit");
+
+           //# Input price and Quantity
+//var price_of_item1 = 650;
+//var price_of_item2 = 1300;
+
+            //  % shai ni arai//
+
+//var total_marks = 980;
+//var marks_obtained =804;
+//var sum = (total_marks/marks_obtained);
+//alert (sum*100);    
+
+
+                   //DollAr//
+
+//var usDollar1 =104.80;
+//var multiplication = (usDollar1 * totalDoller);
+//var saudiRiyal1 = 28;
+//var totalSaudiRiyal = 25;
+//var multiply = (saudiRiyal1 * totalSaudiRiyal);
+//alert(multiplication + multiply);
+
+//var num = 5;
+//++num
+//alert(num);
+//var x =(num*10);
+//alert(x);
+//var y =(x/2);
+//alert(y);
+
+                     //BIRTHDAY?/
+//var currentYear = 2016;
+//var birthdayYear = 1992;
+//var age =(currentYear-birthdayYear);
+//alert(" They are either 23 0r " + age +" years old ." );
+
+             //LFE TIME CALCULATOR//
+    //var favSnacks ="LAys"; 
+    //var amountSnacks = 3;
+    //var currentAge = 15;
+    //var maxAge = 65;
+//var sub =(maxAge-currentAge);
+   //alert("You will need "+ sub*amountSnacks +" chocolatatechip to last you untill the ripe old age of 65");
+
+
+        //ASSIGNNT #06//
+   //var a = 10;
+   //++a
+   //alert(a--);
+//var b = 11;
+//++b;
+//alert(b--);
+//var c = 11;
+//alert(c);
+//var x = 11;
+//x--;
+//alert(x);
+
+
+ // 1B PART//
+ //var a = 2;
+// var b = 1;
+
+
+        
+
+
